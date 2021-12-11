ec5717449294c933a11ac0f4a961d42dd989317f
Abul Hassan <abulhsn63@gmail.com>
2021-12-11 23:17:19 +0530

    sample commit

--- EcartApplication/Controllers/HomeController.cs
+++ EcartApplication/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace EcartApplication.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+         //home controller
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
